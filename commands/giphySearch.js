cb91231df21e88e0258bbf96e78951abb3c36a5c
Antonio Culler <aculler47@gmail.com>
2019-10-14 16:50:19 -0400

    add command folder for ease of adding commands later. minor file restructuring

---
+++ commands/giphySearch.js
@@ -0,0 +1,28 @@
+//Global Stuff
+const {giphytoken} = require('../config.json');
+
+//Giphy Stuff
+const GphApiClient = require('giphy-js-sdk-core');
+giphy = GphApiClient(giphytoken);
+
+module.exports = { 
+    name: 'gif', 
+    execute(message, args) {
+        giphy.search('gifs', {"q": `${args}`})
+        .then((response) => {
+            var total = response.data.length;            
+            var index = Math.floor((Math.random() * total));           
+            var final = response.data[index];
+            
+            //send Gif
+            message.channel.send(
+                {
+                    files: [final.images.fixed_height.url]
+                }
+            )
+        }).catch((error) => {
+            console.error(error);
+            console.log("Error From Giphy Search")
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,44 +1,36 @@
 //Global Stuff
 const {prefix, token, giphytoken} = require('./config.json');
+//File Structure 
+const fs = require('fs');
 
 //Discord Stuff
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-//Giphy Stuff
-const GphApiClient = require('giphy-js-sdk-core');
-giphy = GphApiClient(giphytoken);
+//Get Commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('Ready!')
 })
 
-client.on('message', message => { 
-    // console.log(message.content);
-    if(message.content.startsWith(`${prefix}search`)) { //!search
-        let searchTerm = message.content.split(' ')[1];
-        console.log(searchTerm);
-        giphy.search('gifs', {"q": `${searchTerm}`})
-        .then((response) => {
-            var total = response.data.length;            
-            var index = Math.floor((Math.random() * total));           
-            var final = response.data[index];
-            
-            //send Gif
-            message.channel.send(
-                {
-                    files: [final.images.fixed_height.url]
-                }
-            )
-        }).catch(() => {
-            console.log("Error")
-        })
-        
-        
-
-    }
-})
-
 client.login(token);
 
-
+client.on('message', message => { 
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+    if(!client.commands.has(command)) return;
+    try {
+        client.commands.get(command).execute(message, args[0]);
+    }
+    catch(error){
+        console.error(error);        
+    }
+})
