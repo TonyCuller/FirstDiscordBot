27b038dc20a0e4b05b09f4d83fbbe4d1853271bc
Antonio Culler <aculler47@gmail.com>
2019-10-14 14:48:43 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,46 @@
+//Global Stuff
+const {prefix, token, giphytoken} = require('./config.json');
+
+//Discord Stuff
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//Giphy Stuff
+const GphApiClient = require('giphy-js-sdk-core');
+giphy = GphApiClient(giphytoken);
+
+client.once('ready', () => {
+    console.log('Ready!')
+})
+
+client.on('message', message => { 
+    // console.log(message.content);
+    if(message.content.startsWith(`${prefix}search`)) { //!search
+        let searchTerm = message.content.split(' ')[1];
+        console.log(searchTerm);
+        giphy.search('gifs', {"q": `${searchTerm}`})
+        .then((response) => {
+            var total = response.data.length;
+            console.log(total);
+            var index = Math.floor((Math.random() * total));
+            console.log(index);
+            var final = response.data[index];
+            
+            //send Gif
+            message.channel.send(
+                {
+                    files: [final.images.fixed_height.url]
+                }
+            )
+        }).catch(() => {
+            console.log("Error")
+        })
+        
+        
+
+    }
+})
+
+client.login(token);
+
+
